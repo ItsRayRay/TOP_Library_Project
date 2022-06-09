e644f5ea58f13d7c463ce910f5ca2308f07c5bcf
ItsRayRay <r.boerema21@gmail.com>
2022-06-09 17:25:41 +0200

    add extra info to card

--- script.js
+++ script.js
@@ -1,66 +1,38 @@
 const searchButton = document.querySelector("#search__btn")
 
-
-
-
 searchButton.addEventListener("click", function(e) { 
      //search button
     e.preventDefault()
  
     let searchInput = document.querySelector("input").value  // get the value of the input
 
-
-
-
 async function getBooks() {
     const response = await fetch ("http://openlibrary.org/search.json?q="+searchInput)
     const data = await response.json()
 
-    console.log(data.docs)
-
-
 const cardLayout = document.querySelector(".cards")
-
 
 cardLayout.innerHTML = ``
 
-
-
-for (let i = 0; i < 10 ; i++) {
-
+for (let i = 0; i < 50 ; i++) {
 
 cardLayout.innerHTML += `    <div class="card">
-<img src="http://covers.openlibary.org/b/isbn/`+data.docs[i].isbn[0]+`-M.jpg" alt="" class="card__image">
+<img src="" alt="" class="card__image">
 <div class="card__content">
     <h3 class="card__title">${data.docs[i].title_suggest}</h3>
     <p class="card__text">Author: ${data.docs[i].author_name}</p>
+    <p class="card__text">Genre: ${data.docs[i].subject[0]}</p>
+    <div>Pages: ${data.docs[i].number_of_pages_median}</div>
 </div>
 <div class="card__info">
     <div>+ Add to Readlist</div>
-    <div>Pages: ${data.docs[i].number_of_pages_median}</div>
+    <div>More info: ${data.docs[i].number_of_pages_median}</div>
 </div>
 </div>`
-
-
-
 }
-
-
-
-
-
-
-
-
-
 }
 getBooks() 
 .catch(err => console.log(err))
-
-
-
-
-
 })
 
 
