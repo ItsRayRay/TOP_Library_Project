ab87550e8429469632034e9e39266a7ada5e15e2
ItsRayRay <r.boerema21@gmail.com>
2022-06-10 15:45:18 +0200

    finish card layout booklist

--- scriptbooklist.js
+++ scriptbooklist.js
@@ -1,13 +1,12 @@
 const modal = document.getElementById("modal")
 const modalBtn = document.getElementById("modalBtn")
 const closeBtn = document.querySelector(".close")
+const cardList = document.querySelector(".cards")  
 
 
 
 modalBtn.addEventListener("click", openModal);
-
 closeBtn.addEventListener("click", closeModal);
-
 
 function openModal() {
     modal.style.display = "block";
@@ -15,4 +14,25 @@ function openModal() {
 
 function closeModal() {
     modal.style.display = "none";
+}
+
+
+for (let i = 0; i < localStorage.length; i++) {
+
+let savedBooks = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+
+cardList.innerHTML +=  `<div class="card">
+<img src="" alt="" class="card__image">
+<div class="card__content">
+    <h3 class="card__title">${savedBooks.title}</h3>
+    <p class="card__text">Author: ${savedBooks.Author}</p>
+    <p class="card__text">Genre: ${savedBooks.Genre}</p>
+    <div>Pages: ${savedBooks.Pages}</div>
+</div>
+<div class="card__info">
+    <div onclick="addReadlist()" class="add__readlist">+ Add to Readlist</div>
+</div>
+</div>`
+
 }
