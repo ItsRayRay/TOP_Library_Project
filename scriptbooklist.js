d997b89bcee651fabcc50c84031bc3928d6ea44d
ItsRayRay <r.boerema21@gmail.com>
2022-06-10 15:50:23 +0200

    update card styling

--- scriptbooklist.js
+++ scriptbooklist.js
@@ -22,17 +22,24 @@ for (let i = 0; i < localStorage.length; i++) {
 let savedBooks = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
 
-cardList.innerHTML +=  `<div class="card">
+cardList.innerHTML +=  `   <div class="card">
 <img src="" alt="" class="card__image">
 <div class="card__content">
-    <h3 class="card__title">${savedBooks.title}</h3>
-    <p class="card__text">Author: ${savedBooks.Author}</p>
-    <p class="card__text">Genre: ${savedBooks.Genre}</p>
-    <div>Pages: ${savedBooks.Pages}</div>
+    <h3 class="card__title">${savedBooks.title} </h3>
+    <p class="card__text">${savedBooks.Author} </p>
+    <p class="card__text">Genre:  ${savedBooks.Genre}</p>
+    <div>Pages:  ${savedBooks.Pages}</div>
+    <div class="on__page">On page: 0</div>
+   
 </div>
+<div class="page__number"><form action=""><input id="pagenumbervalue" placeholder= "Pagenumber "type="text"></form></div>
+
+
 <div class="card__info">
-    <div onclick="addReadlist()" class="add__readlist">+ Add to Readlist</div>
+    <div id="readBtn">Read</div> <div id="deleteBtn">Delete</div> 
 </div>
-</div>`
+</div>
+
+`
 
 }
