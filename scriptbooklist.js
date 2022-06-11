7dff9552a5f605a8aec0fca9cc2c58b7b28a3aaa
ItsRayRay <r.boerema21@gmail.com>
2022-06-11 12:38:06 +0200

    update script

--- scriptbooklist.js
+++ scriptbooklist.js
@@ -1,10 +1,8 @@
+
 const modal = document.getElementById("modal")
 const modalBtn = document.getElementById("modalBtn")
 const closeBtn = document.querySelector(".close")
 const cardList = document.querySelector(".cards")  
-let inputValue = document.querySelector("#pagenumbervalue")
-let savedBooksArr = []
-const submitBtn = document.querySelector("#submitBtn")
 
 
 
@@ -22,102 +20,40 @@ function closeModal() {
 
 for (let i = 0; i < localStorage.length; i++) {
 
-    let savedBooks = JSON.parse(localStorage.getItem(localStorage.key(i)));
+let savedBooks = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
 
-    savedBooksArr.push(savedBooks);
-
-}
-
-
-for (let i = 0; i < savedBooksArr.length; i++) {
-    cardList.innerHTML +=  `   <div class="card">
+cardList.innerHTML +=  `   <div class="card">
 <img src="" alt="" class="card__image">
 <div class="card__content">
-    <h3 class="card__title">${savedBooksArr[i].title} </h3>
-    <p class="card__text">${savedBooksArr[i].Author} </p>
-    <p class="card__text">Genre:  ${savedBooksArr[i].Genre}</p>
-    <div>Pages:  ${savedBooksArr[i].Pages}</div>
-   
-    <div>
-    <form>
-  
-</form>
-    </div>
+    <h3 class="card__title">${savedBooks.title} </h3>
+    <p class="card__text">${savedBooks.Author} </p>
+    <p class="card__text">Genre:  ${savedBooks.Genre}</p>
+    <div>Pages:  ${savedBooks.Pages}</div>
+ 
    
 </div>
 
 <div class="card__info">
-<div id="deleteBtn">Read</div>  <div onclick="deleteBtn(${i})" id="deleteBtn">Delete</div>     
+    <div onclick="readBook(${savedBooks.isbn} id="readBtn">Read</div> <div onclick="deleteBook(${savedBooks.isbn})":  id="deleteBtn">Delete</div> 
 </div>
-</div>`
-
-;
-}
-
-
-
-
-function deleteBtn(i) {
-    localStorage.removeItem(localStorage.key(i));
-}
-
-
-
-
-submitBtn.addEventListener("click", function(e) {
-e.preventDefault();
-
-
-
-let titleBook = document.querySelector("#title").value;
-let authorBook =  document.querySelector("#author").value;
-let genreBook =  document.querySelector("#genre").value;
-let pagesBook = document.querySelector("#pages").value;
-
-
-let savedBookObj = {
-    title: titleBook,
-    Author: authorBook,
-    Genre: genreBook,
-    Pages: pagesBook,
-    isbn: "",
-}
-
-
-// saves book in object that will be pushed into the array of savedBooksArr change this to local storage and set key to ISBN number
-
-
-savedBooksArr.push(savedBookObj);
-
-cardList.innerHTML += ""
-
-
-for (let i = 0; i < savedBooksArr.length; i++) {
-    cardList.innerHTML +=  `   <div class="card">
-<img src="" alt="" class="card__image">
-<div class="card__content">
-    <h3 class="card__title">${savedBooksArr[i].title} </h3>
-    <p class="card__text">${savedBooksArr[i].Author} </p>
-    <p class="card__text">Genre:  ${savedBooksArr[i].Genre}</p>
-    <div>Pages:  ${savedBooksArr[i].Pages}</div>
-   
-    <div>
-    <form>
-  
-</form>
-    </div>
-   
 </div>
+`
 
-<div class="card__info">
-<div id="deleteBtn">Read</div>  <div onclick="deleteBtn(${i})" id="deleteBtn">Delete</div>     
-</div>
-</div>`
 
 }
 
+function deleteBook (isbnKey) {
+    console.log(isbnKey)
+    localStorage.removeItem(isbnKey)
+    location.reload()
 
+    fetch ()
 
-})
+}
+
+function readBook (isbnKey) {
     
+    localStorage.setItem(isbnKey, true) 
+    location.reload()
+}
